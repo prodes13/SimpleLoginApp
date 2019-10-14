8436ff2e21894859257479f7cdc2c8a9b8626088
prodes13 <ciobotaru.florin@gmail.com>
2019-10-14 15:00:30 +0300

    first commit

---
+++ client/src/components/Register.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react'
+import { register } from './UserFunctions';
+
+export default class Register extends Component {
+    constructor() {
+        super();
+        this.state = {
+            first_name: '',
+            last_name: '',
+            email: '',
+            password: ''
+        }
+
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onChange(e) {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        const user = {
+            first_name: this.state.first_name,
+            last_name: this.state.last_name,
+            email: this.state.email,
+            password: this.state.password
+        }
+
+        register(user).then(res => {
+            this.props.history.push(`/login`);
+        });
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6 mt-5 mx-auto">
+                        <form noValidate onSubmit={this.onSubmit}>
+                            <h1 className="h3 mb-3 font-weight-normal">Register</h1>
+                            <div className="form-group">
+                                <label htmlFor="first_name">First name</label>
+                                <input type="first_name"
+                                    className="form-control"
+                                    name="first_name"
+                                    placeholder="Enter your first name"
+                                    value={this.state.first_name}
+                                    onChange={this.onChange}
+                                />
+                                <label htmlFor="last_name">Last name</label>
+                                <input type="last_name"
+                                    className="form-control"
+                                    name="last_name"
+                                    placeholder="Enter your last name"
+                                    value={this.state.last_name}
+                                    onChange={this.onChange}
+                                />
+                                <label htmlFor="email">Email Address</label>
+                                <input type="email"
+                                    className="form-control"
+                                    name="email"
+                                    placeholder="Enter your email"
+                                    value={this.state.email}
+                                    onChange={this.onChange}
+                                />
+                                <label htmlFor="password">Password</label>
+                                <input type="password"
+                                    className="form-control"
+                                    name="password"
+                                    placeholder="Enter your password"
+                                    value={this.state.password}
+                                    onChange={this.onChange}
+                                />
+                            </div>
+                            <button type="submit"
+                                className="btn btn-lg btn-primary btn-block">
+                                Register
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
